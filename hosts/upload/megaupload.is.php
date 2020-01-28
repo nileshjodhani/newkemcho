32ff5986d2a9cfa338e8253e57c0f8fdb75e6494
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-01-28 14:55:43 +0530

    Update megaupload.is.php

--- hosts/upload/megaupload.is.php
+++ hosts/upload/megaupload.is.php
@@ -14,15 +14,15 @@
 	$upfiles = upfile($url['host'], 0, $url['path'], 0, 0, 0, $lfile, $lname, 'file','', 0, 0, 0, $url['scheme']);
 	echo "<script type='text/javascript'>document.getElementById('progressblock').style.display='none';</script>\n";
 	
-  #$anonn = strpbrk($upfiles,'{');
-  #$anonnn = json_decode($anonn,true);
-  #$anonnnn = $anonnn['data']['file']['url']['short'];
-  #if(empty($anonnn['data']['file']['url']['short'];)){
-#		html_error('[50x1]Error: Plugin Update Required');
-  #}
-  #$akgh = "https://api.telegram.org/bot".$tgtoken."/sendmessage?chat_id=".$tgchatid."&text=".$anonnnn;
-  #$akju = cURL($akgh);
-  #$download_link = $anonnnn
+  $anonn = strpbrk($upfiles,'{');
+  $anonnn = json_decode($anonn,true);
+  $anonnnn = $anonnn['data']['file']['url']['short'];
+  if(empty($anonnn['data']['file']['url']['short'];)){
+		html_error('[50x1]Error: Plugin Update Required');
+  }
+  $akgh = "https://api.telegram.org/bot".$tgtoken."/sendmessage?chat_id=".$tgchatid."&text=".$anonnnn;
+  $akju = cURL($akgh);
+  $download_link = $anonnnn;
         
         
         
